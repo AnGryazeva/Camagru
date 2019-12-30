dbf0e61e3a6afef407e6c88cab6a1382fc4c07f3
Federation Craydle <fcraydle@vo-d4.21-school.ru>
2019-12-30 13:02:06 +0300

    ok

---
+++ application/core/view.php
@@ -0,0 +1,8 @@
+<?php
+class View
+{
+    public function generate($content_view, $template_view, $data = null)
+    {
+        require 'application/views/' . $template_view;
+    }
+}
---
+++ application/views/404_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8">
+    <link href="/css/404_view.css" rel="stylesheet">
+</head>
+<title> Page Error </title>
+<body>
+<div id = "upper">
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    <script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
+    <a id = "up" href="signin1.php"><p id = "login"> Log in</p></a>
+</div>
+<br/>
+
+<div>
+   <h1> Sorry, this page isn't available. </h1>
+    <h3>The link you followed may be broken, or the page may have been removed.</h3>
+</div>
+
+</body>
+</html>
